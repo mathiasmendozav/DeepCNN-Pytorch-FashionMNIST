6fc0ef878b7713cc028eb6d1caa47aaad3f45b2d
mathiasmendozav <mathiasmendozavargas1403@gmail.com>
2024-10-15 14:52:19 -0700

    model architecture done

--- src/model.py
+++ src/model.py
@@ -4,40 +4,44 @@
 import torch
 import torch.nn as nn
 
-# Model class
 class CNNModel(nn.Module):
-    def __init(self, input_shape: int, hidden_units: int, output_shape: int):
+    def __init__(self, input_channels: int = 1, output_shape: int = 10):
         super(CNNModel, self).__init__()
         
-        # conv blocks
+        # First convolutional block
         self.block1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape,
-                                out_channels=hidden_units,
-                                kernel_size=3,
-                                padding=1),
+            nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(hidden_units),
-            nn.Conv2d(in_channels=hidden_units,
-                                out_channels=hidden_units,
-                                kernel_size=3,
-                                padding=1),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(hidden_units),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.25)
         )
-        
+        # Second convolutional block
         self.block2 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape,
-                                out_channels=hidden_units,
-                                kernel_size=3,
-                                padding=1),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(hidden_units),
-            nn.Conv2d(in_channels=hidden_units,
-                                out_channels=hidden_units,
-                                kernel_size=3,
-                                padding=1),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(hidden_units),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.25)
         )
+        # Fully connected layers
+        self.fully_connected_layer = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=128 * 7 * 7, out_features=256),
+            nn.ReLU(),
+            nn.BatchNorm1d(256),
+            nn.Dropout(0.5),
+            nn.Linear(in_features=256, out_features=output_shape)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.fully_connected_layer(x)
+        return x
